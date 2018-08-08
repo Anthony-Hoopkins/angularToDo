b65d8c2afdb4f2162d802e7ab880c054be6b1c21
Anthony-Hoopkins <34210687+Anthony-Hoopkins@users.noreply.github.com>
2018-08-08 16:51:39 +0300

    remake

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,112 +2,40 @@ import {Component, Directive, ElementRef, HostListener, Output, Renderer2} from 
 
 const dateForInp = new Date();
 const currentDate = `${dateForInp.getFullYear()}-${dateForInp.getMonth() + 1 < 10 ? '0' + (dateForInp.getMonth() + 1) :  dateForInp.getMonth() + 1}-${dateForInp.getDate() < 10 ? '0' + (dateForInp.getDate()) :  dateForInp.getDate()}`;
-let todoListArr = [];
-todoListArr = JSON.parse(localStorage.getItem('todoStorage'));
-
-function init() {
-  if (!localStorage.getItem('todoStorage')) {
-    localStorage.setItem('todoStorage', JSON.stringify([]));
-  }
-  if (!localStorage.getItem('basket')) {
-    localStorage.setItem('basket', JSON.stringify([]));
-  }
-}
-init();
+const todoStorage = 'todoStorage';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './html/app.component.html',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @Output() ready: any;
   title = 'angularToDo';
-  clicks = 0;
-  //
-  //
+  todoListArr = JSON.parse(localStorage.getItem(todoStorage)) || [];
   taskText = '';
   deadLine = currentDate;
-  todoListArr = todoListArr;
   addNewRow() {
-    if (this.taskText && this.taskText.trim() !== ''  && this.deadLine !== '') {
-      todoListArr.unshift({id: +new Date(), ready: false, text: this.taskText, deadLine: this.deadLine});
-      localStorage.setItem('todoStorage', JSON.stringify(this.todoListArr));
-      this.taskText = '';
+    if (!this.taskText || this.taskText.trim() === '' || this.deadLine === '') {
+      return;
     }
+      this.todoListArr.unshift({text: this.taskText, ready: false,  deadLine: this.deadLine});
+      localStorage.setItem(todoStorage, JSON.stringify(this.todoListArr));
+      this.taskText = '';
   }
   addFromEnter(e) {
     if (e.keyCode === 13) {
-      this.addNewRow();  // addBtn.dispatchEvent(new Event('click'));
+      this.addNewRow();
     }
   }
-  onChanged(increased: any) {
-    increased === true ? this.clicks++ : this.clicks--;
+  changedReady(index) {
+    this.todoListArr[index]['ready'] = !this.todoListArr[index]['ready'];
+    localStorage.setItem(todoStorage, JSON.stringify(this.todoListArr));
   }
-  // changedReady(ready: any) {
-  changedReady(e) {
-    todoListArr.forEach( (prop, i) => {
-      if (prop.id === +e.target.closest('.row-list').id) {
-        todoListArr[i]['ready'] = !todoListArr[i]['ready'] ? true : false; // Что тут не так,,,,????!!!!
-        localStorage.setItem('todoStorage', JSON.stringify(todoListArr));
-      }
-    });
+  removedItem(index) {
+    this.todoListArr.splice(index, 1);
+    localStorage.setItem(todoStorage, JSON.stringify(this.todoListArr));
   }
-  removedItem(e) {
-    todoListArr.forEach( (prop, i) => {
-      if (prop.id === +e.target.closest('.row-list').id) {
-        todoListArr.splice(i, 1);
-        localStorage.setItem('todoStorage', JSON.stringify(this.todoListArr));
-      }
-    });
-  }
-  saveEditt(e) {
-    localStorage.setItem('todoStorage', JSON.stringify(todoListArr));
+  saveEditt() {
+    localStorage.setItem(todoStorage, JSON.stringify(this.todoListArr));
   }
 }
-
-
-
-
-// import { ChildComponent } from './child.component';
-import { RowComponent } from './row.component';
-
-    // todoListArr.forEach( (prop, i) => {
-    //   if (prop.id === +e.target.closest('.row-list').id) {
-    //   }
-    // });
-
-
-
-
-
-
-  // const nodes = [].slice.call( e.target.closest('.table-list').children );  //  Array.prototype
-    // console.log(nodes);
-
-
-// const rowSelect = todoListArr[i];
-// rowSelect.ready = rowSelect.ready ? false : true;
-// todoListArr.splice(i, 1, rowSelect);
-
-
-//   }
-
-
-// function fillTodoDisplay( todoListArr ) { todoListArr.forEach( prop => fillRow(prop)) }
-
-// function fillRow(data) {
-//
-//   const newRow = rowExampl.cloneNode(true);
-//   newRow.classList.remove('hidden');
-//   newRow.setAttribute('data-id', data.id);
-//   newRow.setAttribute('data-ready', data.ready);
-//
-//   if (data.ready) {
-//     newRow.querySelector('.info-field').classList.add('ready-class');
-//   }
-//   newRow.querySelector('.text-line').innerHTML = data.text;
-//   newRow.querySelector('.date-line').innerHTML = data.deadLine;
-//   display.insertBefore(newRow , display.firstChild);
-//
-// }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { Input} from '@angular/core';
 
-import {/*AddDirective,*/ AppComponent} from './app.component';
-import { ChildComponent } from './child.component';
-import { RowComponent} from './row.component';
-import { CountComponent } from './count.component';
-import { IfExamplComponent } from './ifExampl.component';
-import { ForExamplComponent } from './componentForExampl/forExampl.component';
-import { NgNgComponent } from './ngNg/app.ngNg';
+import { AppComponent} from './app.component';
+import { RowComponent} from './rowComponent/row.component';
 
 @NgModule({
   declarations: [
-    AppComponent, ChildComponent, RowComponent, CountComponent, IfExamplComponent,  NgNgComponent,
-    ForExamplComponent, /*AddDirective*/
+    AppComponent,  RowComponent
   ],
   imports: [
     BrowserModule, FormsModule
