33921527475faad0b26798f65072e9d7fc3e6172
Anthony-Hoopkins <34210687+Anthony-Hoopkins@users.noreply.github.com>
2018-08-09 11:47:22 +0300

    first commit from ws

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { Input} from '@angular/core';
 
 import { AppComponent} from './app.component';
 import { RowComponent} from './rowComponent/row.component';
