eb5c090d41dbc66d946bfebc73c7a1cafc639860
Anthony-Hoopkins <34210687+Anthony-Hoopkins@users.noreply.github.com>
2018-09-18 19:19:19 +0300

    remake url bg

--- src/app/myMinLen.validator.ts
+++ src/app/myMinLen.validator.ts
@@ -8,6 +8,7 @@ export function myMinLen(min: number): ValidatorFn {
     return {
       myMinLength: {
         requiredLength: min,
+        // actualLength: control.value.length,// тут скорее всего надо подобрать жизненный цикл компонента.
         errorMessage: ` Мин ${min} символов в описании задачи. `
       }
     };
